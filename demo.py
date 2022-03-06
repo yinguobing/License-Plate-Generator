8a6f06d8e28051801eb054db4b9cd3cf29834f0d
yinguobing <yinguobing@gmail.com>
2022-03-06 09:06:21 +0800

    group all the patterns

--- demo.py
+++ demo.py
@@ -1,18 +1,17 @@
 """Generate random license plate images."""
 import cv2
 
-import patterns
 from license_plate import LPImageGenerator
+from patterns.china import BLUE
 
 if __name__ == "__main__":
     # What the license would be like?
-    pattern_blue = patterns.BLUE
-    generator = LPImageGenerator(pattern_blue)
+    generator = LPImageGenerator(BLUE)
 
     # Generate some license plates.
-    for _ in range(5):
+    for _ in range(100):
         result = generator.random_generate()
 
         # Show the results.
         cv2.imshow('result', result)
-        cv2.waitKey(0)
+        cv2.waitKey(100)
